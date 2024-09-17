8e556ba4fe51bbe20dbe6e732ab5e2cf8ba48ed0
ramankushwaha <ramankushwaha8726@gmail.com>
2024-09-17 11:44:56 +0530

    chnages

---
+++ first.py
@@ -0,0 +1,15 @@
+# print("raman")
+# a=int(input("enter the number :"))
+# b=int(input("enter the second number :"))
+# c=a+b
+# print("the sum of the number is :",c)
+
+a=int(input("enter the number :"))
+b=int(input("enter second number :"))
+c=int(input("enter third number :"))
+if (a>b>c):
+    print(a,"is the gratest number ") 
+elif(b>a>c):print(b," is the gratest number ")
+else:print(c," is the gratest number ")
+
+
---
+++ third.py
@@ -0,0 +1,136 @@
+# this is the example of the list 
+# friendName=["pankaj","raman","anshuman"]
+# to add a new member in the list
+# before
+# print("before",friendName)
+# after  
+# using append for adding 
+# friendName.append("vicky")
+# print("after",friendName)
+
+# to add the name in specific position (indexno.)
+# using insert for adding in specific position
+# friendName.insert(0,"amit")
+# friendName.insert(3,"amit")
+# print("after adding ", friendName)
+
+# to remove the name 
+# friendName.remove("amit")
+# print("after removing",friendName)
+# to clear the list 
+# friendName.clear()
+# print("after clearing",friendName)
+
+# using pop we will remove the item from the list from last if we give the index number then the element will removed 
+# friendName.pop()
+# print(friendName)
+
+# to sort the list 
+# it only short one type of data either it is character or number 
+# friendName.sort()
+# print("after shorting",friendName)
+
+# FOR loops
+# to print the element in the given list 
+# for name in friendName:
+    # print(name)
+
+# to print 1 to 10 
+# x=1 
+# for x in range(11):
+#     print(x)
+# # ager 1 se start krna hai to 
+# for x in range(1,11):
+#     print(x)
+
+# even numbber 
+# for evenNO in range(2,20,2):
+#     print(evenNO)
+
+# even no usinf for and if 
+# for no in range(1,11):
+#     if no % 2 == 0:
+#         print(no)
+
+# tuples used to store the data and the data is changable 
+# sets={"pankaj","anshuman","amit","amit"}
+# print(sets)
+# print(type(sets))
+
+
+# make a list and perform all the events 
+
+# lists=["pankaj","raman"]
+# print(lists)
+# lists.append("vicky")
+# lists.append("anshuman")
+# lists.append("shubham")
+# print("after addming :",lists)
+# lists.sort()
+# print("after dorting :",lists)
+# lists.reverse()
+# print("after reversing :",lists)
+# lists.clear()
+# print("after clearing :",lists)
+
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x)
+#  with the help of datetime we want to calculate the days of our age 
+
+
+
+# to create the new file and add my name also close the file 
+# f= open("raman.txt","w")
+# f.write("my name is raman ")
+# f.close()
+
+# f=open("raman.txt","r")
+# print(f.read())
+
+
+# name= input("enter your name :")
+# subject= input("i am mca student :")
+
+
+# f=open("info.txt","w")
+# f.write(name,subject)
+# print(f.write())
+
+# f=open("info.txt","r")
+# print(f.read())
+
+# ASSIGNMENT 2-------------------------------------------------------------------------------------------------
+# Assignment = excel file create kro aur add kro add name email, no 
+# json file add name email no
+# csb file add name email no
+# -------------------------------------------------------------------------------------------------------------
+
+
+# while loop print 10 uumber and reveerse of it
+# i=10
+# while (i<=10):
+#     print(i)
+#     i=i-1
+
+# FUNCTION IN PYTHON
+# CREATING a function to print statement 
+# def myFunction(x,y):
+#     z=x+y
+#     print("add : ",z)
+# call the function by name
+# myFunction(2 ,4)
+
+# finding area 
+# x=int(input("enter the number :"))
+# y=int(input("enter the number :"))
+
+# def area(x,y):
+#     z=x*y
+#     print("area is ",z)
+# area(x,y)
+
+# ASSIGNMENT --------------------------------------------------
+# FUNCTION FOR N   FACTORIAL
+# upload this repositry on the github account 
