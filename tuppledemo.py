7370d880f728f996d4d76a6a888e251f1a550268
ramankushwaha <ramankushwaha8726@gmail.com>
2024-09-17 12:52:24 +0530

    changes

---
+++ tuppledemo.py
@@ -0,0 +1,29 @@
+# mytuple=("hero","villen","sj")
+# print(type(mytuple))
+# print(mytuple)
+# print(mytuple[1])
+# mytuple[1]="raman"
+# print(mytuple)
+
+# for value in mytuple:
+#     print(mytuple)
+# for item in mytuple:
+#     print(mytuple)
+
+# dict it can store multiple data in key value pair for example name email number etc.
+
+# student={
+#     "name":"raman",
+#     "email":"ramansjcvsjdcbh",
+#     "number":12
+# }
+# print(student["number"])
+
+class AgeCalculater:
+    birthYear=int(input("enter your birth year :"))
+    currentYear=int(input("enter todays year :"))
+    age=currentYear-birthYear
+# age=AgeCalculater
+yourAge=AgeCalculater.age
+print("your age is ",yourAge)
+    
